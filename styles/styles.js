425c67f0ca4e16d606959a555a567c16f815347e
tufanbutuner <tufanbutuner@gmail.com>
2022-01-12 19:33:46 +0000

    next code cleanup, added basic styling

---
+++ styles/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import styledComponents from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    font-size: 36px;
+  }
+`;
+Container.displayName = "Container";
