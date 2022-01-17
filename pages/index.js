cef583573192878bde23f3f09ed06a25bb97a6fb
tufanbutuner <tufanbutuner@gmail.com>
2022-01-17 21:02:49 +0000

    Set up homepage using tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,135 @@
-import { Box, Button, Grommet, Heading } from "grommet";
+import {
+  BookmarkAltIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  CursorClickIcon,
+  MenuIcon,
+  PhoneIcon,
+  PlayIcon,
+  RefreshIcon,
+  ShieldCheckIcon,
+  SupportIcon,
+  ViewGridIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { Popover, Transition } from "@headlessui/react";
 
+import { ChevronDownIcon } from "@heroicons/react/solid";
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+const solutions = [
+  {
+    name: "Analytics",
+    description:
+      "Get a better understanding of where your traffic is coming from.",
+    href: "#",
+    icon: ChartBarIcon,
+  },
+  {
+    name: "Engagement",
+    description: "Speak directly to your customers in a more meaningful way.",
+    href: "#",
+    icon: CursorClickIcon,
+  },
+  {
+    name: "Security",
+    description: "Your customers' data will be safe and secure.",
+    href: "#",
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: "Integrations",
+    description: "Connect with third-party tools that you're already using.",
+    href: "#",
+    icon: ViewGridIcon,
+  },
+  {
+    name: "Automations",
+    description:
+      "Build strategic funnels that will drive your customers to convert",
+    href: "#",
+    icon: RefreshIcon,
+  },
+];
+const callsToAction = [
+  { name: "Watch Demo", href: "#", icon: PlayIcon },
+  { name: "Contact Sales", href: "#", icon: PhoneIcon },
+];
+const resources = [
+  {
+    name: "Help Center",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    href: "#",
+    icon: SupportIcon,
+  },
+  {
+    name: "Guides",
+    description:
+      "Learn how to maximize our platform to get the most out of it.",
+    href: "#",
+    icon: BookmarkAltIcon,
+  },
+  {
+    name: "Events",
+    description:
+      "See what meet-ups and other events we might be planning near you.",
+    href: "#",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Security",
+    description: "Understand how we take your privacy seriously.",
+    href: "#",
+    icon: ShieldCheckIcon,
+  },
+];
+const recentPosts = [
+  { id: 1, name: "Boost your conversion rate", href: "#" },
+  {
+    id: 2,
+    name: "How to use search engine optimization to drive traffic to your site",
+    href: "#",
+  },
+  { id: 3, name: "Improve your customer experience", href: "#" },
+];
+
 export default function Home() {
-  const theme = {
-    global: {
-      font: {
-        family: "Inter",
-        size: "14px",
-        height: "20px",
-      },
-    },
-  };
-
   return (
-    <Grommet theme={theme}>
-      <Head>
-        <title>Take Note</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Popover className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt=""
+              />
+            </a>
+            b
+          </div>
 
-      <Box direction="column" pad="xlarge" justify="center" align="center">
-        <Heading level="3" margin="none">
-          Welcome to <span style={{ color: "blue" }}>TakeNote</span>
-        </Heading>
-
-        <h3>Enter task here</h3>
-        <input type="text" placeholder="Enter"></input>
-        <Button primary label="label" />
-      </Box>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/tufanbutuner"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Tufan Butuner
-        </a>
-      </footer>
-    </Grommet>
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <a
+              href="#"
+              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Sign in
+            </a>
+            <a
+              href="#"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-500"
+            >
+              New Task
+            </a>
+          </div>
+        </div>
+      </div>
+    </Popover>
   );
 }
