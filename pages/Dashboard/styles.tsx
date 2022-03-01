ee66c72007302542136923c90d56406fdf3e6741
tufanbutuner <tufanbutuner@gmail.com>
2022-03-01 01:50:19 +0000

    styling changes to dashboard body, tasks

--- pages/Dashboard/styles.tsx
+++ pages/Dashboard/styles.tsx
@@ -4,12 +4,25 @@ export const Task = styled.span`
   display: block;
   @import url("https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap");
   font-family: "Space Grotesk", sans-serif;
+  margin-top: 16px;
+  background: rgb(0, 0, 0, 0.1);
+  border-radius: 6px;
+  padding: 6px 12px;
 `;
 Task.displayName = "Task";
 
 export const DashboardContainer = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap");
   font-family: "Space Grotesk", sans-serif;
+
+  @media (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+
+    img {
+      visibility: hidden;
+    }
+  }
 
   display: grid;
   grid-template-columns: 50% 50%;
@@ -29,7 +42,7 @@ export const TaskListContainer = styled.div`
   grid-area: tasks;
   display: block;
   width: 100%;
-  margin: auto;
+  margin-top: 32px;
   justify-content: center;
   padding: 12px;
 `;
